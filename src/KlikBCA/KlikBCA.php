b919feb390aa7a409a043bc51ac0fddcb37c1d9f
Ammar Faizi <ammarfaizi2@gmail.com>
2017-10-28 13:03:25 +0700

    Create KlikBCA class

---
+++ src/KlikBCA/KlikBCA.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace KlikBCA;
+
+/**
+ * @author Ammar Faizi <ammarfaizi2@gmail.com>
+ * @license MIT
+ * @version 0.0.1
+ */
+final class KlikBCA
+{	
+
+	/**
+	 * @var string
+	 */
+	private $user;
+
+	/**
+	 * @var string
+	 */
+	private $pass;
+
+	/**
+	 * Constructor.
+	 *
+	 * @param string $user
+	 * @param string $pass
+	 */
+	public function __construct($user, $pass)
+	{
+
+	}
+
+	/**
+	 * @return null
+	 */
+	public function __debugInfo()
+	{
+		return null;
+	}
+}
