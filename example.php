5791a924861745f369147a0844b11201216fb6c9
Ammar Faizi <ammarfaizi2@gmail.com>
2017-10-28 13:18:09 +0700

    Prepare example deeply

--- example.php
+++ example.php
@@ -1,2 +1,10 @@
 <?php
 require "vendor/autoload.php";
+
+use KlikBCA\KlikBCA;
+
+$cred = json_decode(file_get_contents("a.tmp"), true);
+
+
+$st = new KlikBCA($cred['user'], $cred['pass']);
+print $st->login();
