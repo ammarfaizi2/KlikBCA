ammarfaizi2/KlikBCA

commit 4f7bd064ceac67ec10ff1390cc95fd65e6796357

Refactor everything
Refactor KlikBCA class and run.php file. KlikBCA file contains the core
of this scraper library and run.php file contains the usage example of
this library.

In this refactoring, we have so many improvements, especially for the
logout part. Previously, when we use this library, our KlikBCA session
will be left dangling and can't be used. This makes our account cannot
be accessed for 10 minutes (waiting for the dangling session to be
expired). After this refactoring, this issue is fixed by invoking the
logout endpoint in the KlikBCA class' destructor.

Thanks to Kurniawan for discussing this with me 4 years ago. See the
link below for the story.

Link: https://fb.com/2159278187435164
Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

src/KlikBCA/KlikBCA.php
@@ -2,6 +2,8 @@
 
 namespace KlikBCA;
 
+use Exception;
+
 /**
  * @author Ammar Faizi <ammarfaizi2@gmail.com>
  * @license MIT
@@ -10,6 +12,11 @@ namespace KlikBCA;
  */
 final class KlikBCA
 {
+	/**
+	 * @const string
+	 */
+	private const USER_AGENT = "Opera/9.80 (J2ME/MIDP; Opera Mini/4.2.14912/35.5706; U; id) Presto/2.8.119 Version/11.10";
+
 	/**
 	 * @var string
 	 */
@@ -26,19 +33,76 @@ final class KlikBCA
 	private $cookieFile;
 
 	/**
-	 *
+	 * @var string
+	 */
+	private $error = "";
+
+	/**
+	 * @var bool
+	 */
+	private $sessActive = false;
+
+	/**
 	 * Constructor.
 	 *
 	 * @param string $username
 	 * @param string $password
-	 * @param string $cookieFile
-	 * @return void
+	 * @param ?string $cookieFile
+	 * @throws \Exception
 	 */
-	public function __construct($username, $password, $cookieFile = null)
+	public function __construct($username, $password, $cookieFile = NULL)
 	{
+		if (!is_string($username))
+			throw Exception("Username must be a string");
+
+		if (!is_string($password))
+			throw Exception("Password must be a string");
+
+		if (!is_string($cookieFile))
+			$cookieFile = getcwd()."/klikbca_cookie.txt";
+
+		touch($cookieFile);
+		$cookieFile = realpath($cookieFile);
+		if (!is_string($cookieFile))
+			throw Exception("Invalid cookiefile");
+
+		if (!is_writable($cookieFile))
+			throw Exception("Cookie file is not writable: {$cookieFile}");
+
 		$this->username = $username;
 		$this->password = $password;
-		$this->cookieFile = is_null($cookieFile) ? getcwd()."/cookie.txt" : $cookieFile;
+		$this->cookieFile = $cookieFile;
+	}
+
+	/**
+	 * Destructor.
+	 *
+	 * Make sure we are logged out to prevent stale session.
+	 */
+	public function __destruct()
+	{
+		if ($this->sessActive)
+			$this->logout();
+
+		@unlink($this->cookieFile);
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function logout()
+	{
+		$o = $this->curl("https://m.klikbca.com/authentication.do?value(actions)=logout", [
+			CURLOPT_POST       => true,
+			CURLOPT_POSTFIELDS => ""
+		]);
+		if (self::isCurlErr($o)) {
+			$err = self::buildCurlErr($o);
+			return false;
+		}
+
+		$this->sessActive = false;
+		return true;
 	}
 
 	/**
@@ -46,28 +110,123 @@ final class KlikBCA
 	 */
 	public function login()
 	{
-		$s = $this->exec("https://m.klikbca.com/login.jsp");
-		$s = $s["out"];
-		if (preg_match_all("/<input.+>/Us", $s, $m)) {
-			$posts["value(user_id)"] = $this->username;
-			$posts["value(pswd)"] = $this->password;
-			$posts["value(Submit)"] = "LOGIN";
-			foreach ($m[0] as $v) {
-				if (preg_match("/hidden/i", $v) && preg_match("/name=\"(.*)\"/Us", $v, $m)) {
-					if (preg_match("/value=\"(.*)\"/Us", $v, $n)) {
-						$posts[html_entity_decode($m[1], ENT_QUOTES, "UTF-8")] = html_entity_decode($n[1], ENT_QUOTES, "UTF-8");
-					} else {
-						$posts[html_entity_decode($m[1], ENT_QUOTES, "UTF-8")] = "";
-					}
-				}
-			}
-			$s = $this->exec("https://m.klikbca.com/authentication.do",
-				[
-					CURLOPT_POST => true,
-					CURLOPT_POSTFIELDS => http_build_query($posts)
-				]
-			);
+		$err = "";
+		$m = [];
+
+		$o = $this->curl("https://m.klikbca.com/login.jsp");
+		if (self::isCurlErr($o)) {
+			$err = self::buildCurlErr($o);
+			goto out_err;
 		}
+		$o = $o["out"];
+
+		if (!preg_match_all("/<input.+>/Us", $o, $m)) {
+			$err = "Cannot find <input> tags on the login page";
+			goto out_err;
+		}
+
+		$posts = [
+			"value(user_id)" => $this->username,
+			"value(pswd)"    => $this->password,
+			"value(Submit)"  => "LOGIN"
+		];
+
+		/*
+		 * Catch all hidden inputs.
+		 */
+		foreach ($m[0] as $v) {
+			if (!preg_match("/hidden/i", $v))
+				/*
+				 * This is not a hidden input, skip it!
+				 */
+				continue;
+
+			if (!preg_match("/name=\"(.*)\"/Us", $v, $m)) {
+				$err = "Cannot get the input tag name";
+				goto out_err;
+			}
+
+			$key = html_entity_decode($m[1], ENT_QUOTES, "UTF-8");
+			if (preg_match("/value=\"(.*)\"/Us", $v, $m))
+				$val = html_entity_decode($m[1], ENT_QUOTES, "UTF-8");
+			else
+				$val = "";
+
+			$posts[$key] = $val;
+		}
+
+		$o = $this->curl("https://m.klikbca.com/authentication.do", [
+			CURLOPT_POST       => true,
+			CURLOPT_POSTFIELDS => http_build_query($posts)
+		]);
+		if (self::isCurlErr($o)) {
+			$err = self::buildCurlErr($o);
+			goto out_err;
+		}
+
+		if (!preg_match("/accountstmt.do\?value/", $o["out"])) {
+			/*
+			 * Maybe wrong username / password?
+			 */
+			$err = "Login failed";
+			goto out_err;
+		}
+
+		$this->sessActive = true;
+		return true;
+
+	out_err:
+		$this->setErr($err);
+		return false;
+	}
+
+	/**
+	 * @return ?array
+	 */
+	public function balanceInquiry()
+	{
+		if (!$this->sessActive) {
+			if (!$this->login())
+				return NULL;
+		}
+
+		$o = $this->curl("https://m.klikbca.com/balanceinquiry.do", [
+			CURLOPT_POST       => true,
+			CURLOPT_POSTFIELDS => ""
+		]);
+		if (self::isCurlErr($o)) {
+			$err = self::buildCurlErr($o);
+			goto out_err;
+		}
+
+		$o = $o["out"];
+		if (!preg_match("/<td><font size='1' color='#.+'><b>(.*)<\/td>/Us", $o, $m)) {
+			$err = "Cannot find appropriate tags (1)";
+			goto out_err;
+		}
+		$accountNum = $m[1];
+
+		if (!preg_match("/<td width='5%'><font size='1' color='#.+'><b>(.*)<\/td>/U", $o, $m)) {
+			$err = "Cannot find appropriate tags (2)";
+			goto out_err;
+		}
+		$currency = $m[1];
+
+		if (!preg_match("/<td align='right'><font size='1' color='#.+'><b>(.*)<\/td>/U", $o, $m)) {
+			$err = "Cannot find appropriate tags (3)";
+			goto out_err;
+		}
+		$amount = $m[1];
+
+		return [
+			"account_number"    => $accountNum,
+			"currency"          => $currency,
+			"available_balance" => $amount
+		];
+
+	out_err:
+		$this->setErr($err);
+		return NULL;
 	}
 
 	/**
@@ -75,89 +234,105 @@ final class KlikBCA
 	 */
 	public function accountStatement($startDate, $endDate = null)
 	{
+		$err = "";
+		$o = $this->curl("https://m.klikbca.com/accountstmt.do?value(actions)=acct_stmt", [
+			CURLOPT_POST       => true,
+			CURLOPT_POSTFIELDS => ""
+		]);
+		if (self::isCurlErr($o)) {
+			$err = self::buildCurlErr($o);
+			goto out_err;
+		}
+		$o = $o["out"];
 
-		$s = $this->exec("https://m.klikbca.com/accountstmt.do?value(actions)=acct_stmt",
-				[
-					CURLOPT_POST => true
-				]
-			);
-		$s = $s["out"];
-		
-		// file_put_contents("c.tmp", $s);die;
-		// $s = file_get_contents("aaaa.tmp");
-		
-		preg_match("/<input type=\"hidden\" name=\"as_fid\" value=\"(.*)\"/Us", $s, $m);
+		if (!preg_match_all("/<input.+>/Us", $o, $m)) {
+			$err = "Cannot find <input> tags on the account statement page";
+			goto out_err;
+		}
+
 		$st = strtotime($startDate);
 		$ed = $endDate ? strtotime($endDate) : $st;
+		if ($ed < $st) {
+			$tmp = $ed;
+			$ed  = $st;
+			$st  = $tmp;
+		}
+
 		$posts = [
-			"r1" => "1",
-			"value(D1)" => "0",
+			"r1"             => "1",
+			"value(D1)"      => "0",
 			"value(startDt)" => date("d", $st),
 			"value(startMt)" => date("m", $st),
 			"value(startYr)" => date("Y", $st),
-			"value(endDt)" => date("d", $ed),
-			"value(endMt)" => date("m", $ed),
-			"value(endYr)" => date("Y", $ed)
+			"value(endDt)"   => date("d", $ed),
+			"value(endMt)"   => date("m", $ed),
+			"value(endYr)"   => date("Y", $ed)
 		];
 
-		if (isset($m[1])) {
-			$posts["as_fid"] = $m[1];
+		/*
+		 * Catch all hidden inputs.
+		 */
+		foreach ($m[0] as $v) {
+			if (!preg_match("/hidden/i", $v))
+				/*
+				 * This is not a hidden input, skip it!
+				 */
+				continue;
+
+			if (!preg_match("/name=\"(.*)\"/Us", $v, $m)) {
+				$err = "Cannot get the input tag name";
+				goto out_err;
+			}
+
+			$key = html_entity_decode($m[1], ENT_QUOTES, "UTF-8");
+			if (preg_match("/value=\"(.*)\"/Us", $v, $m))
+				$val = html_entity_decode($m[1], ENT_QUOTES, "UTF-8");
+			else
+				$val = "";
+			$posts[$key] = $val;
 		}
 
-		$s = $this->exec("https://m.klikbca.com/accountstmt.do?value(actions)=acctstmtview",
-				[
-					CURLOPT_POST => true,
-					CURLOPT_POSTFIELDS => http_build_query($posts)	
-				]
-			);
-		$s = $s["out"];
+		$o = $this->curl("https://m.klikbca.com/accountstmt.do?value(actions)=acctstmtview", [
+			CURLOPT_POST => true,
+			CURLOPT_POSTFIELDS => http_build_query($posts)
+		]);
+		if (self::isCurlErr($o)) {
+			$err = self::buildCurlErr($o);
+			goto out_err;
+		}
+		$o = $o["out"];
 
-		// file_put_contents("b.tmp", $s["out"]);
-		// $s = file_get_contents("b.tmp");
 
-		$data = [];
-		if (preg_match("/<table width=\"100%\" class=\"blue\">(.*)<\/table>/Us", $s, $m)) {
-			preg_match_all("/<tr bgcolor='#.{6}'><td valign='top'>(.*)<\/td><td>(.*)<\/td>/Us", $m[1], $m);
-			foreach ($m[2] as $v) {
-				$v = explode("<br>", $v);
-				$c = explode("<td valign='top'>", $v[$cc = count($v) - 1], 2);
-				unset($v[$cc]);
-				$data[] = [
-					"type" => $c[1],
-					"amount" => $c[0],
-					"info" => implode(array_map(function($v){
-						return trim(html_entity_decode($v, ENT_QUOTES, "UTF-8"));
-					}, $v)," ")
-				];
-			}
+		if (!preg_match("/<table width=\"100%\" class=\"blue\">(.*)<\/table>/Us", $o, $m)) {
+			$err = "Cannot find table on the account statement page";
+			goto out_err;
+		}
+
+		if (!preg_match_all("/<tr bgcolor='#.{6}'><td valign='top'>(.*)<\/td><td>(.*)<\/td>/Us", $m[1], $m)) {
+			$err = "Cannot parse table on the account statement page";
+			goto out_err;
+		}
+
+		foreach ($m[2] as $v) {
+			$v  = explode("<br>", $v);
+			$cc = count($v) - 1;
+			$c  = explode("<td valign='top'>", $v[$cc], 2);
+			unset($v[$cc]);
+
+			foreach ($v as &$vp)
+				$vp = trim(html_entity_decode($vp, ENT_QUOTES, "UTF-8"));
+
+			$data[] = [
+				"type"   => $c[1],
+				"amount" => $c[0],
+				"info"   => implode(" ", $v)
+			];
 		}
 		return $data;
-	}
 
-	/**
-	 * @return array
-	 */
-	public function balanceInquiry()
-	{
-		$s = $this->exec("https://m.klikbca.com/balanceinquiry.do",
-				[
-					CURLOPT_POST => true
-				]
-			);
-		$s = $s["out"];
-		preg_match("/<td><font size='1' color='#0000a7'><b>(.*)<\/td>/Us", $s, $m);
-		$accountNum = $m[1];
-		preg_match("/<td width='5%'><font size='1' color='#0000a7'><b>(.*)<\/td>/U", $s, $m);
-		$currency = $m[1];
-		preg_match("/<td align='right'><font size='1' color='#0000a7'><b>(.*)<\/td>/U", $s, $m);
-		$amount = $m[1];
-		return [
-			[
-				"account_number" => $accountNum,
-				"currency" => $currency,
-				"amount" => $amount
-			]
-		];
+	out_err:
+		$this->setErr($err);
+		return NULL;
 	}
 
 	/**
@@ -165,32 +340,71 @@ final class KlikBCA
 	 * @param array  $opt
 	 * @return array
 	 */
-	private function exec($url, $opt = [])
+	private function curl($url, $opt = [])
 	{
 		$ch = curl_init($url);
-		$optf = [
-			CURLOPT_RETURNTRANSFER => true,
-			CURLOPT_SSL_VERIFYPEER => false,
-			CURLOPT_SSL_VERIFYHOST => false,
-			CURLOPT_FOLLOWLOCATION => true,
-			CURLOPT_USERAGENT => "Opera/9.80 (J2ME/MIDP; Opera Mini/4.2.14912/35.5706; U; id) Presto/2.8.119 Version/11.10",
-			CURLOPT_COOKIEFILE => $this->cookieFile,
-			CURLOPT_COOKIEJAR => $this->cookieFile
+		$optDef = [
+			CURLOPT_RETURNTRANSFER	=> true,
+			CURLOPT_USERAGENT	=> self::USER_AGENT,
+			CURLOPT_COOKIEJAR	=> $this->cookieFile,
+			CURLOPT_COOKIEFILE	=> $this->cookieFile
 		];
-		foreach ($opt as $key => $value) {
-			$optf[$key] = $value;
-		}
-		curl_setopt_array($ch, $optf);
-		$out = curl_exec($ch);
+
+		foreach ($opt as $k => $v)
+			$optDef[$k] = $v;
+
+		curl_setopt_array($ch, $optDef);
+		$out  = curl_exec($ch);
 		$info = curl_getinfo($ch);
-		$error = curl_error($ch);
-		$errno = curl_errno($ch);
+		$err  = curl_error($ch);
+		$ern  = curl_errno($ch);
 		curl_close($ch);
 		return [
-			"out" => $out,
-			"info" => $info,
-			"error" => $error,
-			"errno" => $errno
+			"out"	=> $out,
+			"info"	=> $info,
+			"err"	=> $err,
+			"ern"	=> $ern
 		];
+	}
+
+	/**
+	 * @param string $msg
+	 */
+	private function setErr($msg)
+	{
+		$this->error = $msg;
+	}
+
+	/**
+	 * @param array $o
+	 * @return bool
+	 */
+	private static function isCurlErr($o)
+	{
+		if (!isset($o["ern"], $o["err"]))
+			return false;
+
+		return $o["ern"] || $o["err"];
+	}
+
+	/**
+	 * @param array $o
+	 * @return string
+	 * @throws \Exception
+	 */
+	private static function buildCurlErr($o)
+	{
+		if (!isset($o["ern"], $o["err"]))
+			throw \Exception("Unknown error");
+
+		return sprintf("(%d): %s", (int)$o["ern"], (string)$o["err"]);
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getErr()
+	{
+		return $this->error;
 	}
 }

run.php
@@ -1,17 +1,68 @@
 <?php
 
-require __DIR__."/vendor/autoload.php";
+require __DIR__."/src/KlikBCA/KlikBCA.php";
+
+/**
+ * This contains $username and $password
+ */
 require __DIR__."/credentials.tmp";
 
-$st = new KlikBCA\KlikBCA($username, $password, __DIR__."/cookiea.tmp");
-$st->login();
-$balance = $st->balanceInquiry();
-$accountStatement = $st->accountStatement("2018-05-07", "2018-05-13");
+define("COOKIE_FILE", __DIR__."/cookie.tmp");
 
-print json_encode(
-	[
-		"balance_inquiry" => $balance,
-		"account_statement" => $accountStatement
-	]
-	, 128 | JSON_UNESCAPED_SLASHES
-);
+/**
+ * Show the account balance information.
+ *
+ * @param string $username
+ * @param string $password
+ * @return bool
+ */
+function show_balance($username, $password)
+{
+	$bca = new KlikBCA\KlikBCA($username, $password, COOKIE_FILE);
+	$ret = $bca->login();
+	if (!$ret)
+		goto err;
+
+	$ret = $bca->balanceInquiry();
+	if (!$ret)
+		goto err;
+
+	printf("Balance information:\n%s\n\n",
+               json_encode($ret, JSON_PRETTY_PRINT));
+	return;
+
+err:
+	printf("Error: %s\n", $bca->getErr());
+}
+show_balance($username, $password);
+
+/**
+ * Show account statements given the date range.
+ *
+ * @param string $username
+ * @param string $password
+ * @param string $startDate
+ * @param string $endDate
+ * @return bool
+ */
+function show_account_statements($username, $password, $startDate, $endDate)
+{
+	$bca = new KlikBCA\KlikBCA($username, $password, COOKIE_FILE);
+	$ret = $bca->login();
+	if (!$ret)
+		goto err;
+
+	$ret = $bca->accountStatement($startDate, $endDate);
+	if (!$ret)
+		goto err;
+
+	printf("Account statements for %s to %s:\n%s\n\n", $startDate, $endDate,
+	       json_encode($ret, JSON_PRETTY_PRINT|JSON_UNESCAPED_SLASHES));
+	return;
+
+err:
+	printf("Error: %s\n", $bca->getErr());
+}
+$startDate = "2022-04-25";
+$endDate   = "2022-05-01";
+show_account_statements($username, $password, $startDate, $endDate);

commit 436b04c0188e425c160d60f40cfa351bd50780df

KlikBCA: Fix missing `sessActive` check in `accountStatement()`
If accountStatement() is called when sessActive is false, it will fail
immediately because we don't have the login session yet. Make sure we
call login() if we don't have the login session. Also, update the
annotation comment to reflect the fact that accountStatement() may
return NULL.

Fixes: 4f7bd064ceac67ec10ff1390cc95fd65e6796357 ("Refactor everything")
Signed-off-by: Alviro Iskandar Setiawan <alviro.iskandar@gnuweeb.org>

src/KlikBCA/KlikBCA.php
@@ -231,10 +231,15 @@ final class KlikBCA
 	}
 
 	/**
-	 * @return array
+	 * @return ?array
 	 */
 	public function accountStatement($startDate, $endDate = null)
 	{
+		if (!$this->sessActive) {
+			if (!$this->login())
+				return NULL;
+		}
+
 		$err = "";
 		$o = $this->curl("https://m.klikbca.com/accountstmt.do?value(actions)=acct_stmt", [
 			CURLOPT_POST       => true,