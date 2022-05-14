ammarfaizi2/KlikBCA

commit d06d7bc8f6d3c8f3d45bf678b892762ff2a71b64

KlikBCA: Introducing `setProxy()` method
Make it more convenience to use proxy for all requests. This is a
method to set proxy for the cURL request. To unset the proxy, call
this function with no argument or a NULL argument.

Signed-off-by: Alviro Iskandar Setiawan <alviro.iskandar@gnuweeb.org>

src/KlikBCA/KlikBCA.php
@@ -44,6 +44,11 @@ final class KlikBCA
 	private $sessActive = false;
 
 	/**
+	 * @var ?string
+	 */
+	private $proxy = NULL;
+
+	/**
 	 * Constructor.
 	 *
 	 * @param string $username
@@ -342,6 +347,21 @@ final class KlikBCA
 	}
 
 	/**
+	 * A method to set proxy for the cURL request. To unset the proxy,
+	 * call this function with no argument or a NULL argument.
+	 *
+	 * @param string $proxy
+	 * @return ?string
+	 */
+	public function setProxy($proxy = NULL)
+	{
+		if (is_string($proxy))
+			$this->proxy = $proxy;
+		else
+			$this->proxy = NULL;
+	}
+
+	/**
 	 * @param string $url
 	 * @param array  $opt
 	 * @return array
@@ -355,6 +375,9 @@ final class KlikBCA
 			CURLOPT_COOKIEJAR	=> $this->cookieFile,
 			CURLOPT_COOKIEFILE	=> $this->cookieFile
 		];
+
+		if (is_string($this->proxy))
+			$optDef[CURLOPT_PROXY] = $this->proxy;
 
 		foreach ($opt as $k => $v)
 			$optDef[$k] = $v;

commit 688735105f7937260d7d2cfc8a8cc9288762ccfc

KlikBCA: Fix return annotation comment in `setProxy()`
This function doesn't return anything, the return value annotation
should be "void".

Fixes: d06d7bc8f6d3c8f3d45bf678b892762ff2a71b64 ("KlikBCA: Introducing `setProxy()` method")
Cc: Alviro Iskandar Setiawan <alviro.iskandar@gnuweeb.org>
Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

src/KlikBCA/KlikBCA.php
@@ -351,7 +351,7 @@ final class KlikBCA
 	 * call this function with no argument or a NULL argument.
 	 *
 	 * @param string $proxy
-	 * @return ?string
+	 * @return void
 	 */
 	public function setProxy($proxy = NULL)
 	{