ammarfaizi2/KlikBCA

commit 688735105f7937260d7d2cfc8a8cc9288762ccfc

KlikBCA: Fix return annotation comment in `setProxy()`
This function doesn't return anything, the return value annotation
should be "void".

Fixes: d06d7bc8f6d3c8f3d45bf678b892762ff2a71b64 ("KlikBCA: Introducing `setProxy()` method")
Cc: Alviro Iskandar Setiawan <alviro.iskandar@gnuweeb.org>
Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

src/KlikBCA/KlikBCA.php
@@ -351,7 +351,7 @@ final class KlikBCA
 	 * call this function with no argument or a NULL argument.
 	 *
 	 * @param string $proxy
-	 * @return ?string
+	 * @return void
 	 */
 	public function setProxy($proxy = NULL)
 	{

commit 000a66f358e2e1117e0b9cc4d55d56d61ab2e2a5

KlikBCA: Fix error handling in the `logout()` method
The error handling on curl error was wrong, it should set `$this->err`,
not `$err`. Setting in `$err` unused local variable doesn't mean
anything. This was a no-op then return false. What we should do is
setting `$this->err` then return false, so the caller can get the
error message via `getErr()` method.

Fixes: 688735105f7937260d7d2cfc8a8cc9288762ccfc ("KlikBCA: Fix return annotation comment in `setProxy()`")
Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

src/KlikBCA/KlikBCA.php
@@ -103,7 +103,7 @@ final class KlikBCA
 			CURLOPT_POSTFIELDS => ""
 		]);
 		if (self::isCurlErr($o)) {
-			$err = self::buildCurlErr($o);
+			$this->err = self::buildCurlErr($o);
 			return false;
 		}
 